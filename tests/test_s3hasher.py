d035cd109169b1837925fedb671cec3c9c4e8e81
Joe Moore <jpvlsmv@gmail.com>
2018-07-18 14:51:54 -0400

    Test module-as-import code

--- tests/test_s3hasher.py
+++ tests/test_s3hasher.py
@@ -10,3 +10,8 @@ def test_main():
 
     assert result.output == '()\n'
     assert result.exit_code == 0
+
+import subprocess
+def test_module():
+    rc = subprocess.check_call(['python','-ms3hasher'])
+    assert rc == 0
